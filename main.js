008fba7ba0bd831b2018caf834a55b6c9501350a
Priya Hiremath <priyahrish18@gmail.com>
2018-03-07 17:43:59 +1300

    added Canvas to index.html

---
+++ main.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("hello!");
+    
+draw();
+
+
+
+
+
+
+    });
+
+   /* console.log("Hello!");
+var  my_canvas = document.getElementById("canvas");
+console.log("hello!");
+var context = "30px Garamond";
+
+context.fillText("hello!" , 15, 175);
+
+
+/*
+var context = my_canvas.msGetInputContext();
+console.log("hello again!");
+ context.beginPath();
+console.log("hello again");
+context.arc(95,85,40,0, 2*Math.PI);
+context.stroke();
+console.log("this is last one!");
+context.fillRect(10,10,50,20);
+context.fillStyle = "blue";
+*/
+
+function draw() {
+    console.log("hello again!");
+    var canvas = document.getElementById('canvas');
+    console.log("another hello!");
+    if (canvas.getContext) {
+       var ctx = canvas.getContext('2d');
+  
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+      ctx.stroke();
+      
+
+    }
+  }
+
+
